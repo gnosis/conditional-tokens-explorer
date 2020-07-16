6b2064892a4c91ff12322e4c58ee1d962974537f
Mariano Aguero <mariano.aguero@altoros.com>
2020-07-16 16:30:16 -0300

    Create new route

--- src/pages/connected/index.tsx
+++ src/pages/connected/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+
 import { PrepareCondition } from '../prepare-condition'
 import { SplitConditionContainer } from '../split-condition'
 import { ConditionsList } from 'pages/conditions-list'
+import { ConditionsDetailContainer } from 'pages/condition-detail'
 import { PositionsList } from 'pages/positions-list'
 
 export const Connected = () => {
@@ -11,6 +13,7 @@ export const Connected = () => {
       <Route component={PrepareCondition} exact path="/" />
       <Route component={SplitConditionContainer} exact path="/split/" />
       <Route component={ConditionsList} exact path="/conditions" />
+      <Route component={ConditionsDetailContainer} exact path="/conditions/:conditionId" />
       <Route component={PositionsList} exact path="/positions" />
     </Switch>
   )
