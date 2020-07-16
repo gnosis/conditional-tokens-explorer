93da627055d13e0b8889d02a2c5944f368cd306d
Mariano Aguero <mariano.aguero@altoros.com>
2020-07-16 16:23:53 -0300

    Add new declarations

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,7 @@
 declare module '@walletconnect/web3-provider'
 declare type Maybe<T> = T | null
 declare module 'apollo-link-logger'
+declare module 'react-copy-to-clipboard'
+declare module '@realitio/realitio-lib/formatters/question'
+declare module '@realitio/realitio-lib/formatters/template'
+declare type KnownOracle = 'kleros' | 'realitio' | 'unknown'
