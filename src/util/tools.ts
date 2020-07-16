a1df8b46564fdebe94c93003f65ea266a46198cb
Mariano Aguero <mariano.aguero@altoros.com>
2020-07-16 16:26:08 -0300

    Add new tool functions

--- src/util/tools.ts
+++ src/util/tools.ts
@@ -1,5 +1,8 @@
 import { ethers } from 'ethers'
 import { Provider } from 'ethers/providers'
+import moment from 'moment-timezone'
+
+import { BYTES_REGEX } from '../config/constants'
 
 export const isAddress = (address: string) => {
   try {
@@ -17,4 +20,35 @@ export const isContract = async (provider: Provider, address: string): Promise<b
 
 export const range = (max: number): number[] => {
   return Array.from(new Array(max), (_, i) => i)
+}
+
+export const isConditionIdValid = (conditionId: string): boolean => BYTES_REGEX.test(conditionId)
+
+export const truncateStringInTheMiddle = (
+  str: string,
+  strPositionStart: number,
+  strPositionEnd: number
+) => {
+  const minTruncatedLength = strPositionStart + strPositionEnd
+  if (minTruncatedLength < str.length) {
+    return `${str.substr(0, strPositionStart)}...${str.substr(
+      str.length - strPositionEnd,
+      str.length
+    )}`
+  }
+  return str
+}
+
+export const getConditionTypeTitle = (templateId: Maybe<number>) => {
+  if (templateId === 5 || templateId === 6) {
+    return 'Nuanced Binary'
+  } else if (templateId === 2) {
+    return 'Categorical'
+  } else {
+    return 'Binary'
+  }
+}
+
+export const formatDate = (date: Date): string => {
+  return moment(date).tz('UTC').format('YYYY-MM-DD - HH:mm [UTC]')
 }
