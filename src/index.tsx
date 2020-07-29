dcaf743468b364f3c1efa86960a995ef43098e5b
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-07-29 10:15:08 -0300

    (add) base layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import { Main } from './pages/main'
 import { Web3ContextProvider } from './contexts/Web3Context'
 import { ApolloProviderWrapper } from './contexts/Apollo'
 import { HashRouter as Router } from 'react-router-dom'
-import { Header } from './components/common/Header'
+import { Header, Footer } from './components/layout'
+import { MainScroll, MainWrapper, InnerContainer } from './components/pureStyledComponents/layout'
+import { ThemeProvider } from 'styled-components'
+import 'sanitize.css'
+import theme from './theme'
+import { GlobalStyle } from './theme/global_style'
 
 ReactDOM.render(
   <React.StrictMode>
     <Web3ContextProvider>
       <ApolloProviderWrapper>
-        <Router>
-          <Header />
-          <Main />
-        </Router>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router>
+            <MainWrapper>
+              <Header />
+              <MainScroll>
+                <InnerContainer>
+                  <Main />
+                </InnerContainer>
+                <Footer />
+              </MainScroll>
+            </MainWrapper>
+          </Router>
+        </ThemeProvider>
       </ApolloProviderWrapper>
     </Web3ContextProvider>
   </React.StrictMode>,
