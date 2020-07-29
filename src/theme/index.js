5c62ba815e2ff49e370dd0dd19a5a6d3364e7611
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-07-29 10:14:31 -0300

    (add) styled components theme

---
+++ src/theme/index.js
@@ -0,0 +1,40 @@
+const theme = {
+  fonts: {
+    defaultSize: '14px',
+    fontFamily: `'Averta', 'Helvetica Neue', 'Arial', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', '-apple-system', 'BlinkMacSystemFont', sans-serif`,
+    fontFamilyCode: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
+  },
+  border: {
+    color: '#ECEFF1',
+    borderRadius: '6px',
+  },
+  colors: {
+    error: '#fa0000',
+    mainBodyBackground: '#f3f2f2',
+    primary: '#1565C0',
+    secondary: '#E3F2FD',
+    textColor: '#5d6d74',
+  },
+  header: {
+    backgroundColor: '#fff',
+    height: '60px',
+  },
+  layout: {
+    horizontalPadding: '10px',
+    maxWidth: '1272px',
+  },
+  paddings: {
+    mainPadding: '15px',
+  },
+  themeBreakPoints: {
+    lg: '992px',
+    md: '768px',
+    sm: '480px',
+    xl: '1024px',
+    xs: '320px',
+    xxl: '1280px',
+    xxxl: '1366px',
+  },
+}
+
+export default theme
