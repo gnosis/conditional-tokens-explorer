ba62933328b0c70c6a46d575d370b6ca63e031db
Mariano Aguero <mariano.aguero@altoros.com>
2020-07-30 16:48:56 -0300

    Add outcomes prettier

--- src/hooks/useQuestion.tsx
+++ src/hooks/useQuestion.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import { useWeb3Connected } from '../contexts/Web3Context'
 import { Question } from '../util/types'
 
-export const useQuestion = (questionId: string) => {
+export const useQuestion = (questionId: string, outcomeSlotCount: number) => {
   const { RtioService } = useWeb3Connected()
 
   const [question, setQuestion] = React.useState<Maybe<Question>>(null)
+  const [outcomesPrettier, setOutcomesPrettier] = React.useState<string[]>([])
   const [error, setError] = React.useState(undefined)
   const [loading, setLoading] = React.useState<boolean>(true)
 
@@ -32,8 +33,23 @@ export const useQuestion = (questionId: string) => {
     }
   }, [RtioService, questionId])
 
+  React.useEffect(() => {
+    let cancelled = false
+    if (!cancelled) setLoading(true)
+
+    const outcomesSlots = question ? question.outcomes: Array.from(Array(outcomeSlotCount), (_, i) => i + 1 + '')
+
+    if (!cancelled) setOutcomesPrettier(outcomesSlots)
+    if (!cancelled) setLoading(false)
+
+    return () => {
+      cancelled = true
+    }
+  }, [question])
+
   return {
     question,
+    outcomesPrettier,
     error,
     loading,
   }
