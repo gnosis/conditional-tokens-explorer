d1d64ab378a78c3c3893551edb82f14eba06656b
Mariano Aguero <mariano.aguero@altoros.com>
2020-07-31 15:32:40 -0300

    Add outcome slots probabilities and payouts

--- src/pages/report-payouts/OutcomeSlotsToReport.tsx
+++ src/pages/report-payouts/OutcomeSlotsToReport.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useForm, Controller } from 'react-hook-form'
 import { BigNumber } from 'ethers/utils'
 
+import { BigNumberInputWrapper } from 'components/common/BigNumberInputWrapper'
 import { GetCondition_condition } from '../../types/generatedGQL'
 import { useQuestion } from '../../hooks/useQuestion'
-import { formatBigNumber } from '../../util/tools'
+import { getTokenFromAddress } from '../../config/networkConfig'
+import { useWeb3Connected } from '../../contexts/Web3Context'
+import { Token } from '../../util/types'
+import { ZERO_BN } from '../../config/constants'
+import { divBN } from '../../util/tools'
 
 interface Props {
   condition: GetCondition_condition
@@ -12,44 +18,115 @@ interface Props {
 interface Outcome {
   name: string
   probability: number
-  payout: BigNumber
+}
+
+interface FormInputs {
+  payouts: BigNumber[]
 }
 
 export const OutcomeSlotsToReport = ({ condition }: Props) => {
-  const { questionId, outcomeSlotCount } = condition
+  const { networkConfig } = useWeb3Connected()
+
+  const { questionId, outcomeSlotCount, positions } = condition
+
+  let decimals = 18
+  if (positions) {
+    const { collateralToken } = positions[0]
+    const { id: collateralAddress } = collateralToken
+    const token: Token = getTokenFromAddress(networkConfig.networkId, collateralAddress)
+    decimals = token.decimals
+  }
+
   const { outcomesPrettier } = useQuestion(questionId, outcomeSlotCount)
 
-  const outcomes: Outcome[] = outcomesPrettier.map((outcome, index) => {
-    return {
-      name: outcome,
-      probability: 0,
-      payout: new BigNumber(0),
+  const [outcomes, setOutcomes] = React.useState<Outcome[]>([])
+
+  React.useEffect(() => {
+    let cancelled = false
+    if (outcomes.length === 0) {
+      const outcomes: Outcome[] = outcomesPrettier.map((outcome, index) => {
+        return {
+          name: outcome,
+          probability: 0,
+        }
+      })
+      if (!cancelled) setOutcomes(outcomes)
     }
-  })
+    return () => {
+      cancelled = true
+    }
+  }, [outcomesPrettier, outcomes.length])
+
+  const { getValues, control, handleSubmit } = useForm<FormInputs>({ mode: 'onChange' })
+
+  const onSubmit = (payouts: FormInputs) => {
+    // Validate exist at least one payout
+    console.log(payouts)
+  }
+
+  const onChange = (value: BigNumber, index: number) => {
+    const values = Object.values(getValues())
+
+    // Calculate total payouts entered
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const total = values.reduce((previousValue: any, currentValue: any, currentIndex: number) => {
+      const payout = currentIndex === index ? value : currentValue
+      return previousValue.add(payout)
+    }, new BigNumber(0))
+
+    // Calculate probabilities against the total calculated previously
+    const outcomesValues: Outcome[] = (outcomes as Outcome[]).map((outcome, currentIndex) => {
+      const amount = currentIndex === index ? value : (values[currentIndex] as BigNumber)
+      const probability = divBN(amount, total as BigNumber) * 100
+      return {
+        ...outcome,
+        probability,
+      }
+    })
+
+    // Update the outcomes with the new probabilities
+    setOutcomes(outcomesValues)
+  }
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Outcome</th>
-            <th>Probabilities</th>
-            <th>Payout</th>
-          </tr>
-        </thead>
-        <tbody>
-          {outcomes.map((outcome, index) => {
-            const { name, probability, payout } = outcome
-            return (
-              <tr key={index}>
-                <td>{name}</td>
-                <td>{probability.toFixed(2)}%</td>
-                <td>{formatBigNumber(payout, 18)}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <table>
+          <thead>
+            <tr>
+              <th>Outcome</th>
+              <th>Probabilities</th>
+              <th>Payout</th>
+            </tr>
+          </thead>
+          <tbody>
+            {outcomes.map((outcome, index) => {
+              const { name, probability } = outcome
+              return (
+                <tr key={index}>
+                  <td>{name}</td>
+                  <td>{probability.toFixed(2)}%</td>
+                  <td>
+                    <Controller
+                      control={control}
+                      name={`payouts[${index}]`}
+                      decimals={decimals}
+                      as={BigNumberInputWrapper}
+                      defaultValue={new BigNumber(0)}
+                      rules={{ required: true, validate: (amount) => amount.gte(ZERO_BN) }}
+                      onChange={(value) => {
+                        onChange(value[0], index)
+                        return value[0]
+                      }}
+                    />
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <input type="submit" />
+      </form>
     </>
   )
 }
