e1d04159b93ebc4ca090144ce556cdf81a48baa6
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-08-10 15:34:41 +0200

    Add token GNO to ganache

--- docker/deploy-contracts/prepare_db.sh
+++ docker/deploy-contracts/prepare_db.sh
@@ -15,7 +15,7 @@ MOCK_DAI_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3
 MOCK_USDC_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d' --abi 'constructor(string,uint8)' --args '["USDC", 6]' ERC20.bin | jq -r .address)
 MOCK_OWL_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d' --abi 'constructor(string,uint8)' --args '["OWL", 18]' ERC20.bin | jq -r .address)
 MOCK_WETH_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d' --abi 'constructor(string,uint8)' --args '["WETH", 18]' ERC20.bin | jq -r .address)
-MOCK_CHAI_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d' --abi 'constructor(string,uint8)' --args '["CHAI", 18]' ERC20.bin | jq -r .address)
+MOCK_GNO_ADDRESS=$(eth contract:deploy --network http://ganache:8545 --pk '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d' --abi 'constructor(string,uint8)' --args '["GNO", 18]' ERC20.bin | jq -r .address)
 
 # deploy conditional tokens contracts
 cd conditional-tokens-contracts
@@ -43,6 +43,6 @@ echo "mock dai: ${MOCK_DAI_ADDRESS}" >> contracts_addresses.txt
 echo "mock usdc: ${MOCK_USDC_ADDRESS}" >> contracts_addresses.txt
 echo "mock owl: ${MOCK_OWL_ADDRESS}" >> contracts_addresses.txt
 echo "mock weth: ${MOCK_WETH_ADDRESS}" >> contracts_addresses.txt
-echo "mock chai: ${MOCK_CHAI_ADDRESS}" >> contracts_addresses.txt
+echo "mock gno: ${MOCK_GNO_ADDRESS}" >> contracts_addresses.txt
 
 cat contracts_addresses.txt
