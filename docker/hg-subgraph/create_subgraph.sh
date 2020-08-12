5f40fc7383b187f1ea5960e2fa7bf0b4f0bd25e7
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-08-12 14:05:24 +0200

    Add `graph codegen` to hg-subgraph
      * Hg-subgraph docker container requires to run migrate and graph
        codegen to create the subgraph.yaml file before create the
    `gnosis/hg` subgraph on the local thesubgrap container. The ganache
    specific CT address will be replaced at subgraph.yaml file.

--- docker/hg-subgraph/create_subgraph.sh
+++ docker/hg-subgraph/create_subgraph.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e # exit when any command fails
 
@@ -6,7 +6,25 @@ waitport() {
     while ! nc -z $1 $2 ; do sleep 1 ; done
 }
 
+cd hg-subgraph/
+sed -i 's/localhost/ganache/g' ops/render-subgraph-conf.js
+sed -i 's/localhost/ganache/g' node_modules/@gnosis.pm/conditional-tokens-contracts/truffle.js
+
+waitport ganache 8545
+
+echo "Run migrate"
+npm run migrate
+echo "Run render subgraph"
+npm run refresh-abi && npm run render-subgraph-config-local
+echo "Setup the right CT address"
+sed -i -E "s/(address: '0x[a-zA-Z0-9]+')/address: '0xA57B8a5584442B467b4689F1144D269d096A3daF'/g" subgraph.yaml
+echo "Apply codegen"
+./node_modules/.bin/graph codegen
+
 waitport graph-node 8000
 
 echo "Creating gnosis/hg at http://graph-node:8020"
-graph create --node http://graph-node:8020 gnosis/hg
+./node_modules/.bin/graph create --node http://graph-node:8020 gnosis/hg
+
+echo "Deploying gnosis/hg at local"
+./node_modules/.bin/graph deploy --node http://graph-node:8020 --ipfs http://ipfs:5001 gnosis/hg
