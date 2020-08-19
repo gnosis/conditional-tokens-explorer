42a949ae514c900c6a39e8381af9159eeb5c46ed
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-08-19 17:46:05 -0300

    (add) working modal

--- src/pages/MergePositions/Contents.tsx
+++ src/pages/MergePositions/Contents.tsx
@@ -1,25 +1,26 @@
-import { getLogger } from 'util/logger'
-import { isConditionFullIndexSet, minBigNumber } from 'util/tools'
-import { Status } from 'util/types'
-
-import { Button } from 'components/buttons'
-import { CenteredCard } from 'components/common/CenteredCard'
-import { Row } from 'components/pureStyledComponents/Row'
-import { ZERO_BN } from 'config/constants'
-import { getTokenFromAddress } from 'config/networkConfig'
-import { useConditionContext } from 'contexts/ConditionContext'
-import { useMultiPositionsContext } from 'contexts/MultiPositionsContext'
-import { useWeb3Connected } from 'contexts/Web3Context'
 import { ethers } from 'ethers'
 import { BigNumber } from 'ethers/utils'
 import React, { useCallback, useMemo, useState } from 'react'
-import { ConditionalTokensService } from 'services/conditionalTokens'
-import styled from 'styled-components'
 
+import { Button } from '../../components/buttons/Button'
+import { CenteredCard } from '../../components/common/CenteredCard'
 import { Amount } from '../../components/mergePositions/Amount'
 import { MergePreview } from '../../components/mergePositions/MergePreview'
 import { SelectCondition } from '../../components/mergePositions/SelectCondition'
 import { SelectPosition } from '../../components/mergePositions/SelectPosition'
+import { ButtonContainer } from '../../components/pureStyledComponents/ButtonContainer'
+import { Row } from '../../components/pureStyledComponents/Row'
+import { FullLoading } from '../../components/statusInfo/FullLoading'
+import { IconTypes } from '../../components/statusInfo/common'
+import { ZERO_BN } from '../../config/constants'
+import { getTokenFromAddress } from '../../config/networkConfig'
+import { useConditionContext } from '../../contexts/ConditionContext'
+import { useMultiPositionsContext } from '../../contexts/MultiPositionsContext'
+import { useWeb3Connected } from '../../contexts/Web3Context'
+import { ConditionalTokensService } from '../../services/conditionalTokens'
+import { getLogger } from '../../util/logger'
+import { isConditionFullIndexSet, minBigNumber } from '../../util/tools'
+import { Status } from '../../util/types'
 
 const logger = getLogger('MergePosition')
 
@@ -36,7 +37,8 @@ export const Contents = () => {
     positions,
   } = useMultiPositionsContext()
   const { clearCondition, condition, errors: conditionErrors } = useConditionContext()
-  const [status, setStatus] = React.useState<Maybe<Status>>(null)
+  const [status, setStatus] = useState<Maybe<Status>>(null)
+  const [error, setError] = useState<string | undefined>()
 
   const isFullIndexSet = useMemo(() => {
     return condition && isConditionFullIndexSet(positions, condition)
@@ -117,6 +119,7 @@ export const Contents = () => {
       }
     } catch (err) {
       setStatus(Status.Error)
+      setError(err)
       logger.error(err)
     }
   }, [positions, condition, CTService, amount, clearPositions, clearCondition])
@@ -144,17 +147,21 @@ export const Contents = () => {
       <Row cols="1fr">
         <MergePreview amount={amount} />
       </Row>
-      <ButtonWrapper>
+      {(status === Status.Loading || status === Status.Error) && (
+        <FullLoading
+          actionButton={
+            status === Status.Error ? { text: 'OK', onClick: () => setStatus(null) } : undefined
+          }
+          icon={status === Status.Error ? IconTypes.error : IconTypes.spinner}
+          message={status === Status.Error ? error : 'Working...'}
+          title={status === Status.Error ? 'Error' : 'Merge Positions'}
+        />
+      )}
+      <ButtonContainer>
         <Button disabled={disabled} onClick={onMerge}>
           Merge
         </Button>
-      </ButtonWrapper>
+      </ButtonContainer>
     </CenteredCard>
   )
 }
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 80px;
-`
