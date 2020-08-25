be6c7aa4db7a59465bda01cf7224811134787432
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-08-25 14:59:27 -0300

    (update) icons to receive props

--- src/components/icons/ChevronRight.tsx
+++ src/components/icons/ChevronRight.tsx
@@ -9,8 +9,14 @@ const Wrapper = styled.svg`
   top: auto;
 `
 
-export const ChevronRight: React.FC = (props) => (
-  <Wrapper {...props} height="11.999" width="7" xmlns="http://www.w3.org/2000/svg">
+export const ChevronRight: React.FC<{ className?: string }> = (props) => (
+  <Wrapper
+    className={`chevronRight ${props.className}`}
+    height="11.999"
+    width="7"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <path
       d="M6.71 5.29l-.06-.054L1.707.292A1 1 0 00.293 1.706L4.587 6 .293 10.293a1 1 0 001.414 1.414L6.66 6.756l.05-.047a1.014 1.014 0 000-1.42"
       data-name="chevron right"
