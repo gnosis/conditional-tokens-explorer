40f0846dd4c8f99d0f892811ea374b00eeea93c2
Mariano Aguero <mariano.aguero@altoros.com>
2020-08-25 08:57:24 -0300

    Remove deep

--- src/hooks/useAllowanceState.tsx
+++ src/hooks/useAllowanceState.tsx
@@ -44,7 +44,7 @@ export const useAllowanceState = (allowanceMethods: AllowanceMethods, amount: Bi
 
   useEffect(() => {
     fetchAllowance()
-  }, [fetchAllowance, amount])
+  }, [fetchAllowance])
 
   useEffect(() => {
     setHasUnlockedCollateral(false)
