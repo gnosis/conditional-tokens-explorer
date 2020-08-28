8d6c615e9803fb3a6f56fd9fdd178f32ad54d0e3
Mariano Aguero <mariano.aguero@altoros.com>
2020-08-28 10:28:59 -0300

    Add integration of the env var in the methods

--- src/services/conditionalTokens.ts
+++ src/services/conditionalTokens.ts
@@ -5,6 +5,7 @@ import { BigNumber } from 'ethers/utils'
 import Web3Utils from 'web3-utils'
 
 import { NetworkConfig } from '../config/networkConfig'
+import { CONFIRMATIONS_TO_WAIT } from '../config/constants'
 
 const CTHelpers = CTHelpersConstructor(Web3Utils)
 
@@ -115,7 +116,7 @@ export class ConditionalTokensService {
         gasLimit: 1750000,
       }
     )
-    return this.provider.waitForTransaction(tx.hash)
+    return this.provider.waitForTransaction(tx.hash, CONFIRMATIONS_TO_WAIT)
   }
 
   redeemPositions = async (
@@ -130,7 +131,7 @@ export class ConditionalTokensService {
       conditionId,
       indexSets
     )
-    return this.provider.waitForTransaction(tx.hash)
+    return this.provider.waitForTransaction(tx.hash, CONFIRMATIONS_TO_WAIT)
   }
 
   mergePositions = async (
@@ -147,7 +148,7 @@ export class ConditionalTokensService {
       partition,
       amount
     )
-    return this.provider.waitForTransaction(tx.hash)
+    return this.provider.waitForTransaction(tx.hash, CONFIRMATIONS_TO_WAIT)
   }
 
   async getOutcomeSlotCount(conditionId: string): Promise<BigNumber> {
@@ -179,6 +180,6 @@ export class ConditionalTokensService {
 
   async reportPayouts(questionId: string, payouts: number[]): Promise<TransactionReceipt> {
     const tx = await this.contract.reportPayouts(questionId, payouts)
-    return this.provider.waitForTransaction(tx.hash)
+    return this.provider.waitForTransaction(tx.hash, CONFIRMATIONS_TO_WAIT)
   }
 }
