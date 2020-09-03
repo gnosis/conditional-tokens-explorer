95de1ffc038e4893ec2b35aac01a15e6722ab91b
Mariano Aguero <mariano.aguero@altoros.com>
2020-09-03 00:24:14 -0300

    Add collateral filter dropdown

---
+++ src/components/common/CollateralFilterDropdown/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { useWeb3ConnectedOrInfura } from '../../../contexts/Web3Context'
+import { Token } from '../../../util/types'
+import { ButtonSelectLight } from '../../buttons/ButtonSelectLight'
+import { Dropdown, DropdownItem, DropdownPosition } from '../Dropdown'
+import { TokenIcon } from '../TokenIcon'
+
+interface Props {
+  onClick: (symbol: string, address: string) => void
+  value: string
+}
+
+export const CollateralFilterDropdown = ({ onClick, value }: Props) => {
+  const { networkConfig } = useWeb3ConnectedOrInfura()
+  const tokensList = networkConfig
+    ? [
+        ...networkConfig.getTokens().map((token: Token) => {
+          return {
+            content: <TokenIcon symbol={token.symbol} />,
+            onClick: () => {
+              onClick(token.symbol, token.address)
+            },
+            value: token.symbol,
+          }
+        }),
+      ]
+    : []
+
+  const tokenItems = [
+    {
+      content: 'All Collaterals',
+      onClick: () => {
+        onClick('all', '')
+      },
+      value: 'all',
+    },
+    ...tokensList,
+  ]
+
+  return (
+    <Dropdown
+      currentItem={tokenItems.findIndex((tokenItem) => tokenItem.value === value)}
+      dropdownButtonContent={
+        <ButtonSelectLight
+          content={tokenItems.filter((tokenItem) => tokenItem.value === value)[0].content}
+        />
+      }
+      dropdownPosition={DropdownPosition.right}
+      items={tokenItems.map((tokenItem, index) => (
+        <DropdownItem key={index} onClick={tokenItem.onClick}>
+          {tokenItem.content}
+        </DropdownItem>
+      ))}
+    />
+  )
+}
