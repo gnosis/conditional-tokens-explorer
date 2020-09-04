0899df8ea7c4c10ac1d7c23590447d80f2a05cc1
Mariano Aguero <mariano.aguero@altoros.com>
2020-09-04 18:01:17 -0300

    Fix import

--- src/components/navigation/Mainmenu/index.tsx
+++ src/components/navigation/Mainmenu/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
-import { InnerContainer } from 'components/navigation/Mainmenu/pureStyledComponents/InnerContainer'
+import { InnerContainer } from 'components/pureStyledComponents/InnerContainer'
 
 const Wrapper = styled.nav`
   background-color: rgba(255, 255, 255, 0.8);
