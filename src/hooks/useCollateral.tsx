8c9a553165e5528e23efe5f7ae12e312ff8af555
Maximiliano Molina <hpmaxi@gmail.com>
2020-09-07 17:59:22 -0300

    Use collateral with error and loading

--- src/hooks/useCollateral.tsx
+++ src/hooks/useCollateral.tsx
@@ -4,10 +4,14 @@ import { useWeb3ConnectedOrInfura } from 'contexts/Web3Context'
 import { ERC20Service } from 'services/erc20'
 import { Token } from 'util/types'
 
-export const useCollateral = (collateralAddress: string): Maybe<Token> => {
+export const useCollateral = (
+  collateralAddress: string
+): { collateral: Maybe<Token>; error: Maybe<Error>; loading: boolean } => {
   const { _type: status, networkConfig, provider } = useWeb3ConnectedOrInfura()
 
   const [collateral, setCollateral] = React.useState<Maybe<Token>>(null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState<Maybe<Error>>(null)
 
   React.useEffect(() => {
     let cancelled = false
@@ -25,25 +29,32 @@ export const useCollateral = (collateralAddress: string): Maybe<Token> => {
       }
 
       // try to recover ERC20 profile
-      try {
-        const erc20Service = new ERC20Service(provider, collateral)
-        const token = await erc20Service.getProfileSummary()
-        return token
-      } catch (e) {
-        return null
-      }
+      const erc20Service = new ERC20Service(provider, collateral)
+      const token = await erc20Service.getProfileSummary()
+      return token
     }
 
-    fetchToken(collateralAddress).then((token) => {
-      if (!cancelled) {
-        setCollateral(token)
-      }
-    })
+    setLoading(true)
+    fetchToken(collateralAddress)
+      .then((token) => {
+        if (!cancelled) {
+          setCollateral(token)
+          setLoading(false)
+          setError(null)
+        }
+      })
+      .catch((err) => {
+        if (!cancelled) {
+          setLoading(false)
+          setCollateral(null)
+          setError(err)
+        }
+      })
 
     return () => {
       cancelled = true
     }
   }, [status, provider, collateralAddress, networkConfig])
 
-  return collateral
+  return { loading, error, collateral }
 }
