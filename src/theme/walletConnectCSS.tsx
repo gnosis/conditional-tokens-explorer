b8c39da28faa157fe5b94d92b874cd859fccfc63
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-09-09 16:04:49 -0300

    (add) wallet connect styles

---
+++ src/theme/walletConnectCSS.tsx
@@ -0,0 +1,20 @@
+import { css } from 'styled-components'
+
+export const walletConnectCSS = css`
+  #walletconnect-wrapper {
+    .walletconnect-modal__header p {
+      color: ${(props) => props.theme.colors.darkBlue};
+    }
+    .walletconnect-qrcode__base {
+      background-color: ${(props) => props.theme.modalStyle.overlay.backgroundColor};
+    }
+    .walletconnect-modal__base {
+      border-radius: ${(props) => props.theme.modalStyle.content.borderRadius};
+      box-shadow: ${(props) => props.theme.modalStyle.content.boxShadow};
+    }
+    .walletconnect-qrcode__text {
+      color: ${(props) => props.theme.colors.textColor};
+      font-size: 16px;
+    }
+  }
+`
