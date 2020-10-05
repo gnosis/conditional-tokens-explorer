37ac402ca978379a3f87e63b7c089faa40a0529a
Mariano Aguero <mariano.aguero@altoros.com>
2020-10-05 17:45:54 -0300

    Add `Not Connected to walled` title

--- src/components/form/Amount/index.tsx
+++ src/components/form/Amount/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { BigNumberInputWrapper } from 'components/form/BigNumberInputWrapper'
 import { TitleControlButton } from 'components/pureStyledComponents/TitleControl'
 import { TitleValue } from 'components/text/TitleValue'
+import { Web3ContextStatus, useWeb3ConnectedOrInfura } from 'contexts/Web3Context'
 import { formatBigNumber } from 'util/tools'
 
 interface Props {
@@ -29,14 +30,22 @@ export const Amount = ({
   onUseWalletBalance,
   tokenSymbol,
 }: Props) => {
+  const { _type: status } = useWeb3ConnectedOrInfura()
+
+  const isDisconnected = status !== Web3ContextStatus.Connected
+
   return (
     <TitleValue
       title="Amount"
       titleControl={
-        <TitleControlButton disabled={disabled} onClick={onUseWalletBalance}>
-          Use {isFromAPosition ? 'Position' : 'Wallet'} Balance ($
-          {formatBigNumber(balance, decimals)})
-        </TitleControlButton>
+        isDisconnected || !balance ? (
+          <TitleControlButton disabled>Not Connected To Wallet</TitleControlButton>
+        ) : (
+          <TitleControlButton disabled={disabled} onClick={onUseWalletBalance}>
+            Use {isFromAPosition ? 'Position' : 'Wallet'} Balance ($
+            {formatBigNumber(balance, decimals)})
+          </TitleControlButton>
+        )
       }
       value={
         <BigNumberInputWrapper
