0ae98c26a94c81a1ef1eb001c2bc8c76ff177655
Mariano Aguero <mariano.aguero@altoros.com>
2020-10-07 12:22:41 -0300

    Added new builder

--- src/queries/CTEConditions.tsx
+++ src/queries/CTEConditions.tsx
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag'
+import { OracleFilterOptions, AdvancedFilter, StatusOptions } from 'util/types'
 
 export interface ConditionsListType {
   conditionId?: string
@@ -10,6 +11,15 @@ export const DEFAULT_OPTIONS = {
   conditionId: '',
   oracleIn: [],
   oracleNotIn: [],
+}
+
+export const DEFAULT_OPTIONS_LIST: AdvancedFilter = {
+  ReporterOracle: {
+    type: OracleFilterOptions.All,
+    value: []
+  },
+  Status: StatusOptions.All,
+  MinMaxOutcomes: null
 }
 
 const conditionFragment = gql`
@@ -34,6 +44,42 @@ const conditionFragment = gql`
     }
   }
 `
+export const buildQueryConditionsList = (advancedFilter: AdvancedFilter = DEFAULT_OPTIONS_LIST) => {
+  const { ReporterOracle, Status, MinMaxOutcomes } = advancedFilter
+
+  const whereClauseInternal = [
+    ReporterOracle.type === OracleFilterOptions.Custom ? 'oracle_not_in: $oracleNotIn' : '',
+    ReporterOracle.type === OracleFilterOptions.Current || ReporterOracle.type === OracleFilterOptions.Kleros || ReporterOracle.type === OracleFilterOptions.Realitio ? 'oracle_in: $oracleIn' : '',
+    Status === StatusOptions.Open || Status === StatusOptions.Resolved ? 'resolved: $resolved' : '',
+    MinMaxOutcomes ? 'outcomeSlotCount_lte: $maxOutcome , outcomeSlotCount_gte: $minOutcome' : '',
+  ]
+    .filter((s) => s.length)
+    .join(',')
+
+  const whereClause = whereClauseInternal ? `, where: { ${whereClauseInternal} }` : ''
+
+  const variablesClauseInternal = [
+    ReporterOracle.type === OracleFilterOptions.Custom ? '$oracleNotIn: [String]' : '',
+    ReporterOracle.type === OracleFilterOptions.Current || ReporterOracle.type === OracleFilterOptions.Kleros || ReporterOracle.type === OracleFilterOptions.Realitio ? '$oracleIn: [String]' : '',
+    Status === StatusOptions.Open || Status === StatusOptions.Resolved ? '$resolved: Boolean' : '',
+    MinMaxOutcomes ? '$maxOutcome: Int,$minOutcome: Int' : '',
+  ]
+    .filter((s) => s.length)
+    .join(',')
+
+  const variablesClause = variablesClauseInternal ? `(${variablesClauseInternal})` : ''
+
+  const query = gql`
+      query Conditions ${variablesClause} {
+        conditions(first: 1000 ${whereClause} , orderBy: createTimestamp, orderDirection: desc) {
+          ...ConditionData
+        }
+      }
+      ${conditionFragment}
+  `
+
+  return query
+}
 
 export const buildQueryConditions = (options: ConditionsListType = DEFAULT_OPTIONS) => {
   const { conditionId, oracleIn, oracleNotIn } = options
