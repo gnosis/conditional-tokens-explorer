68b66d4a08cfb9deb70ea3286bb1fbd4828f8b66
Mariano Aguero <mariano.aguero@altoros.com>
2020-10-30 17:39:31 -0300

    Add header

--- src/pages/RedeemPosition/index.tsx
+++ src/pages/RedeemPosition/index.tsx
@@ -4,12 +4,14 @@ import { BatchBalanceProvider } from 'contexts/BatchBalanceContext'
 import { ConditionProvider } from 'contexts/ConditionContext'
 import { MultiPositionsProvider } from 'contexts/MultiPositionsContext'
 import { Contents } from 'pages/RedeemPosition/Contents'
+import { PageTitle } from 'components/pureStyledComponents/PageTitle'
 
 export const RedeemPosition = () => {
   return (
     <MultiPositionsProvider>
       <BatchBalanceProvider checkForEmptyBalance={true}>
         <ConditionProvider checkForConditionNotResolved={true}>
+          <PageTitle>Redeem Positions</PageTitle>
           <Contents />
         </ConditionProvider>
       </BatchBalanceProvider>
