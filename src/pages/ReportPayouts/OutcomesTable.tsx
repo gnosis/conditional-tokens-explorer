50680246d29996748dc9c0dd4d9e066b59023674
Mariano Aguero <mariano.aguero@altoros.com>
2020-11-09 18:45:03 -0300

    Add improvements to the report payout section

--- src/pages/ReportPayouts/OutcomesTable.tsx
+++ src/pages/ReportPayouts/OutcomesTable.tsx
@@ -1,14 +1,7 @@
-import { BigNumber } from 'ethers/utils'
-import React, { useEffect, useState } from 'react'
-import { Controller, FormContextValues } from 'react-hook-form'
+import React, { KeyboardEvent, useMemo } from 'react'
 import styled from 'styled-components'
 
-import { BigNumberInputWrapper } from 'components/form/BigNumberInputWrapper'
-import { ZERO_BN } from 'config/constants'
-import { useQuestion } from 'hooks/useQuestion'
-import { FormInputs } from 'pages/ReportPayouts/Contents'
-import { GetCondition_condition } from 'types/generatedGQLForCTE'
-import { divBN } from 'util/tools'
+import { Textfield } from 'components/pureStyledComponents/Textfield'
 
 const Wrapper = styled.form`
   border-radius: 4px;
@@ -72,7 +65,7 @@ const TD = styled.td<{ textAlign?: string }>`
   text-align: ${(props) => props.textAlign};
 `
 
-const Textfield = styled(BigNumberInputWrapper)`
+const TextfieldProbability = styled(Textfield)`
   margin-left: auto;
 
   input {
@@ -89,6 +82,15 @@ const Textfield = styled(BigNumberInputWrapper)`
     text-align: right;
     width: 70px;
 
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    ::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
     &:focus {
       border-bottom-color: ${(props) => props.theme.colors.primary};
     }
@@ -100,9 +102,10 @@ TH.defaultProps = {
 }
 
 interface Props {
-  condition: GetCondition_condition
-  formMethods: FormContextValues<FormInputs>
-  decimals: number
+  conditionId: string
+  payouts: number[]
+  setPayout: (payout: number, index: number) => void
+  outcomeSlotCount: number
 }
 
 interface Outcome {
@@ -110,53 +113,23 @@ interface Outcome {
   probability: number
 }
 
-export const OutcomesTable = ({ condition, decimals, formMethods }: Props) => {
-  const { outcomeSlotCount, questionId } = condition
-  const { outcomesPrettier } = useQuestion(questionId, outcomeSlotCount)
-  const [outcomes, setOutcomes] = useState<Outcome[]>([])
+export const OutcomesTable = (props: Props) => {
+  const { conditionId, outcomeSlotCount, payouts, setPayout } = props
 
-  const { control, getValues } = formMethods
-
-  useEffect(() => {
-    let cancelled = false
-    if (outcomes.length === 0) {
-      const outcomes: Outcome[] = outcomesPrettier.map((outcome) => {
-        return {
-          name: outcome,
-          probability: 0,
-        }
-      })
-      if (!cancelled) setOutcomes(outcomes)
-    }
-    return () => {
-      cancelled = true
-    }
-  }, [outcomesPrettier, outcomes.length])
-
-  const onChange = (value: BigNumber, index: number) => {
-    const values = Object.values(getValues())
-
-    // Calculate total payouts entered
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const total = values.reduce((previousValue: any, currentValue: any, currentIndex: number) => {
-      const payout = currentIndex === index ? value : currentValue
-      return previousValue.add(payout)
-    }, new BigNumber(0))
-
-    // Calculate probabilities against the total calculated previously
-    const outcomesValues: Outcome[] = (outcomes as Outcome[]).map((outcome, currentIndex) => {
-      const amount = currentIndex === index ? value : (values[currentIndex] as BigNumber)
-      const probability = (total as BigNumber).isZero()
-        ? 0
-        : divBN(amount, total as BigNumber) * 100
-      return {
-        ...outcome,
+  const outcomesWithProbabilities: Outcome[] = useMemo(() => {
+    const outcomes: Outcome[] = []
+    const total = payouts.reduce((previous: number, current: number) => previous + current, 0)
+    for (let index = 0; index < outcomeSlotCount; index++) {
+      const amount = payouts[index] || 0
+      const probability = total > 0 ? (amount / total) * 100 : 0
+      const outcome = {
+        name: index + '',
         probability,
       } as Outcome
-    })
-    // Update the outcomes with the new probabilities
-    setOutcomes(outcomesValues)
-  }
+      outcomes.push(outcome)
+    }
+    return outcomes
+  }, [outcomeSlotCount, payouts])
 
   return (
     <Wrapper>
@@ -169,24 +142,27 @@ export const OutcomesTable = ({ condition, decimals, formMethods }: Props) => {
           </TR>
         </THead>
         <TBody>
-          {outcomes.map((outcome, index) => {
+          {outcomesWithProbabilities.map((outcome, index) => {
             const { name, probability } = outcome
             return (
               <TR key={index}>
                 <TD>{name}</TD>
                 <TD textAlign="right">{probability.toFixed(2)}%</TD>
                 <TD textAlign="right">
-                  <Controller
-                    as={Textfield}
-                    control={control}
-                    decimals={decimals}
-                    defaultValue={new BigNumber(0)}
+                  <TextfieldProbability
+                    key={`${conditionId}_${outcome}_${index}`}
+                    min={0}
                     name={`payouts[${index}]`}
-                    onChange={(value) => {
-                      onChange(value[0], index)
-                      return value[0]
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                      const { value } = event.currentTarget
+                      setPayout(+value, index)
                     }}
-                    rules={{ required: true, validate: (amount) => amount.gte(ZERO_BN) }}
+                    onKeyPress={(event: KeyboardEvent) =>
+                      event.charCode === 8 || event.charCode === 0
+                        ? null
+                        : event.charCode >= 48 && event.charCode <= 57
+                    }
+                    type="number"
                   />
                 </TD>
               </TR>
