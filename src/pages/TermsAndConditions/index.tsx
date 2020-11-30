9ec9e9080efa711021c733fda19ba53f3fe177f6
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-11-30 16:42:08 -0300

    (add) adapt page titile to have extra controls

--- src/pages/TermsAndConditions/index.tsx
+++ src/pages/TermsAndConditions/index.tsx
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 
 import { BaseCard } from 'components/pureStyledComponents/BaseCard'
 import { Li, OrderedList } from 'components/pureStyledComponents/Lists'
-import { PageTitle } from 'components/pureStyledComponents/PageTitle'
 import { PageTitleNote } from 'components/pureStyledComponents/PageTitleNote'
 import { Paragraph } from 'components/pureStyledComponents/Paragraph'
 import { Title } from 'components/pureStyledComponents/Title'
+import { PageTitle } from 'components/text/PageTitle'
 
 const BaseCardGrow = styled(BaseCard)`
   flex-grow: 1;
