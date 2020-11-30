3824932596eb5d8cb6de830e1605917d7b5d98b5
Maximiliano Molina <hpmaxi@gmail.com>
2020-11-30 15:28:12 -0300

    Remove dead code related to conditions queries

--- src/queries/CTEConditions.tsx
+++ src/queries/CTEConditions.tsx
@@ -146,41 +146,6 @@ export const buildQueryConditionsList = (
   return query
 }
 
-export const buildQueryConditions = (options: ConditionsListType = DEFAULT_OPTIONS) => {
-  const { conditionId, oracleIn, oracleNotIn } = options
-
-  const whereClauseInternal = [
-    conditionId ? 'id: $conditionId' : '',
-    oracleIn && oracleIn.length > 0 ? 'oracle_in: $oracleIn' : '',
-    oracleNotIn && oracleNotIn.length > 0 ? 'oracle_not_in: $oracleNotIn' : '',
-  ]
-    .filter((s) => s.length)
-    .join(',')
-
-  const whereClause = whereClauseInternal ? `, where: { ${whereClauseInternal} }` : ''
-
-  const variablesClauseInternal = [
-    conditionId ? '$conditionId: String' : '',
-    oracleIn && oracleIn.length > 0 ? '$oracleIn: [String]' : '',
-    oracleNotIn && oracleNotIn.length > 0 ? '$oracleNotIn: [String]' : '',
-  ]
-    .filter((s) => s.length)
-    .join(',')
-
-  const variablesClause = variablesClauseInternal ? `(${variablesClauseInternal})` : ''
-
-  const query = gql`
-      query Conditions ${variablesClause} {
-        conditions(first: 1000 ${whereClause} , orderBy: createTimestamp, orderDirection: desc) {
-          ...ConditionData
-        }
-      }
-      ${conditionFragment}
-  `
-
-  return query
-}
-
 export const GetConditionQuery = gql`
   query GetCondition($id: ID!) {
     condition(id: $id) {
